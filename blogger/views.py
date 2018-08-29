e480717ddb1f45c97886475808a120f64bd26bb0
Nisha <nisha.sridhar1998@gmail.com>
2018-08-29 21:28:40 +0530

    Changed Html for site

--- blogger/views.py
+++ blogger/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+#A view is a place where we put the "logic" of our application. It will request information from the model you created before and pass it to a template
+
+def post_list(request):
+    return render(request, 'blogger/post_list.html', {})
