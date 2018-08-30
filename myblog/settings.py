27b463bf9e2a0f142080a3fcd9c033a412c8919d
Nisha <nisha.sridhar1998@gmail.com>
2018-08-30 23:29:30 +0530

    added hostname

--- myblog/settings.py
+++ myblog/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com'] #for local host
 
-
+ALLOWED_HOSTS = ['nishasridhar.pythonanywhere.com']
 # Application definition
 
 INSTALLED_APPS = [
